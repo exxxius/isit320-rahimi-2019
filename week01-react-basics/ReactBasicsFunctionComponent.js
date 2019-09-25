0a0b167c26e947ec75949f26761f14bae62765d6
exxxius <mehdi.rahimi@bellevuecollege.edu>
2019-09-24 21:11:38 -0700

    ReactBasics done

---
+++ week01-react-basics/ReactBasicsFunctionComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ReactBasicsFunctionComponent = () => (
+   <h1>An H1 element in a React Function Component</h1>
+);
---
+++ week01-react-basics/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ReactBasicsFunctionComponent } from './ReactBasicsFunctionComponent.js';
+
+ReactDOM.render(<ReactBasicsFunctionComponent/>, document.getElementById('root'));
